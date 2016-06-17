c1808c0349326c7aca2d5514e68247ca33a6a950
Klevd <klev2687@gmail.com>
2016-06-16 17:04:01 -0700

    Working function

--- server.R
+++ server.R
@@ -1,5 +1,22 @@
 library(UsingR)
 library(caret)
+library(shiny)
 data("father.son")
 
-a <- as.numeric(names(which(table(father.son$sheight)>1)))
+#Duplicate son heights values are identified
+shdup <- as.numeric(names(which(table(father.son$sheight)>1)))
+
+#Dataset with no duplicates for son heights
+fsnodup <- father.son[!(father.son$sheight %in% shdup),]
+
+fitlm <- train(sheight~fheight, data = fsnodup, method='lm')
+lmcoeff <- summary(fitlm)$coeff[2,1]
+inter <- summary(fitlm)$coeff[1,1]
+predsheight <- function(fh1) (fh1*lmcoeff)+inter
+
+shinyServer(
+    function(input, output){
+    output$fhi1 <- renderPrint({input$fh1})
+    output$sho1 <- renderPrint({predsheight(input$fh1)})
+    }
+)
