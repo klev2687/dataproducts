2577d3627c7d9f5aec4ddb99882f1f71cd7408cd
KlevD <klev2687@gmail.com>
2016-06-16 19:41:33 -0700

    removed min-max. steps changed to 0.2

--- ui.R
+++ ui.R
@@ -1,7 +1,7 @@
 shinyUI(pageWithSidebar(
         headerPanel("Predicting Sons Height Using Fathers Height"),
         sidebarPanel(
-          numericInput('fh1', 'Enter fathers height in inches', 0, min=0, max=84, step=0.5)
+          numericInput('fh1', 'Enter fathers height in inches', 0, step = 0.2)
 ),
 
 mainPanel(
